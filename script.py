44f083ba3ac92ccf873c816d4eb23b3128224b35
FartyPants <fartypantsham@gmail.com>
2023-05-22 13:47:45 -0400

    Add files via upload

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ def save_text_to_file(text, noteid):
     file_path = Path(file_name)  # Specify the path and name of the output file
     with open(file_path, "w", encoding="utf-8") as file:
         file.write(text)
+       
  
 def load_text(noteid):
     file_name = f"notebook{noteid}.txt"
@@ -13,10 +14,9 @@ def load_text(noteid):
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             default_text = file.read()
-    except:
+    except FileNotFoundError:
         default_text = ""        
     
-    #print("Load:", file_path)    
             
     return default_text
 
@@ -30,31 +30,11 @@ def ui():
    
     def change_textbox(choice):
         global noteid
-        if choice == "Note 2":
-            noteid = 2
-            default_text = load_text(2)
-            return gr.update(value=default_text)
-        elif choice == "Note 3":
-            noteid = 3
-            default_text = load_text(3)
-            return gr.update(value=default_text)
-        elif choice == "Note 4":
-            noteid = 4
-            default_text = load_text(4)
-            return gr.update(value=default_text)
-        elif choice == "Note 5":
-            noteid = 5
-            default_text = load_text(5)
-            return gr.update(value=default_text)
-        elif choice == "Note 6":
-            noteid = 6
-            default_text = load_text(6)
-            return gr.update(value=default_text)
-        else:
-            noteid = 1
-            default_text = load_text(1)
-            return gr.update(value=default_text)
-   
+        noteid = int(choice.split()[1])
+        noteid = min(max(noteid, 1), 6)
+        default_text = load_text(noteid)
+        return gr.update(value=default_text)
+     
 
     default_text = load_text(noteid)    
 
